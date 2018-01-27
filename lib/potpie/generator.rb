42452d65b40bd24e8f12f79411a4ffb76298f812
Justin Holm <jlholmz@gmail.com>
2018-01-27 00:53:05 -0500

    Rename config to config_params

--- lib/potpie/generator.rb
+++ lib/potpie/generator.rb
@@ -3,11 +3,11 @@ module Potpie
     # Config params for Potpie.configuration
     # Base32_secret and user_email should be
     # provided for a code to be generated.
-    attr_reader :config
+    attr_reader :config_params
 
-    def initialize(config)
-      @base32_secret = config.base32_secret
-      @user_email = config.user_email
+    def initialize(config_params)
+      @base32_secret = config_params.base32_secret
+      @user_email = config_params.user_email
     end
 
     # Generates a scannable QR code.
